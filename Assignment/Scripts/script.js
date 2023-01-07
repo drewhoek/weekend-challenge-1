62e63cd88476023a40771043d15dea751bbb7ee2
Drew Hoek <drewh815@gmail.com>
2023-01-07 17:24:43 -0600

    Modified addEmployee function and created getEmployeeInfo function

--- Assignment/Scripts/script.js
+++ Assignment/Scripts/script.js
@@ -2,6 +2,7 @@ console.log('jq and js');
 
 $( function () {
     console.log('in Ready');
+    $('#employeeSubmit').on( 'click', getEmployeeInfo );
 });
 
 let employees = [];
@@ -16,9 +17,36 @@ function addEmployee(firstNameInput, lastNameInput, idInput, titleInput, salaryI
         title: titleInput,
         salary: salaryInput,
     }
+
     // Push that object into the global array of employees]
     employees.push(newEmployeeObject);
-    console.log('current employees:', newEmployeeObject);
+    console.log('current employees:', employees);
     return true;
 } // end addEmployee
-addEmployee('Greg', 'Johnson', 235245, 'Software dev', 75000);
+addEmployee('Greg', 'Johnson', 235245, 'Software dev', 75000); // Testing if addEmployee works
+
+function getEmployeeInfo () {
+    console.log('in getEmployeeInfo');
+    // Define values for inputs within variables
+    const firstNameVal = $('#firstNameInput').val();
+    const lastNameVal = $('#lastNameInput').val();
+    const idNumberVal = Number($('#idInput').val());
+    const titleVal = $('#titleInput').val();
+    const salaryVal = Number($('#salaryInput').val());
+
+    console.log(`First Name: ${firstNameVal}, Last Name: ${lastNameVal}, ID Nummber: ${idNumberVal}, Title: ${titleVal}, Salary: ${salaryVal}`);
+    // Clear input fields
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#salaryInput').val('');
+    // Call addEmployee to push input info to the global array
+    addEmployee(firstNameVal, lastNameVal, idNumberVal, titleVal, salaryVal);
+    // Call append DOM function
+    appendDom();
+}
+
+function appendDom() {
+
+}
