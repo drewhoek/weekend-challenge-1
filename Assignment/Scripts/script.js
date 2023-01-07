51090d9e49de01457b31ab7d5375b83cc10a7e48
Drew Hoek <drewh815@gmail.com>
2023-01-07 16:35:11 -0600

    Got addEmployee function working by calling it within the code

--- Assignment/Scripts/script.js
+++ Assignment/Scripts/script.js
@@ -1,4 +1,24 @@
 console.log('jq and js');
+
 $( function () {
     console.log('in Ready');
 });
+
+let employees = [];
+
+function addEmployee(firstNameInput, lastNameInput, idInput, titleInput, salaryInput){
+    console.log('in addEmployee');
+    // Create new object to house all input information
+    const newEmployeeObject = {
+        firstName: firstNameInput,
+        lastName: lastNameInput,
+        idNumber: idInput,
+        title: titleInput,
+        salary: salaryInput,
+    }
+    // Push that object into the global array of employees]
+    employees.push(newEmployeeObject);
+    console.log('current employees:', newEmployeeObject);
+    return true;
+} // end addEmployee
+addEmployee('Greg', 'Johnson', 235245, 'Software dev', 75000);
